6e75b40c7f03d278886f689c9809f9ede17eed78
Leonid Shuster <lenchaus@gmail.com>
2021-07-22 02:51:44 +0300

    cosmetic fix

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
-import {Modal, Button, Fade, Typography} from '@material-ui/core'
+import {Modal, Button, Typography} from '@material-ui/core'
 import { gotoPage } from '../actions/appActions';
 import CheckoutOrderTable from './CheckoutOrderTable'
 
